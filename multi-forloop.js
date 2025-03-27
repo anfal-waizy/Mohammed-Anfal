1b04a44ee0c8d6c9fc4d30c90905d6c6750c7a41
MohammedAnfal <anfalcanva07@gmail.com>
2025-03-27 16:57:40 +0530

    multi-forloop

---
+++ multi-forloop.js
@@ -0,0 +1,13 @@
+// let n = 10;
+// let result = 1;
+// for (let i = 1; i <= 10; i++) {
+//   result = n * i;
+//   console.log(n + "x" + i + "=" + result);
+// }
+
+let n = 7;
+let result = 1;
+for (let i = 1; i <= 10; i++) {
+  result = n * i;
+  console.log(`${n} x ${i} = ${result}`);
+}
