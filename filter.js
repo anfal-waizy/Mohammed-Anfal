7ac426b6a7eee177f220e60369a772d9edf441d5
MohammedAnfal <anfalcanva07@gmail.com>
2025-04-12 20:28:39 +0530

    filter

--- filter.js
+++ filter.js
@@ -1,4 +1,6 @@
-let sales = [2100, 25000, 17000, 10000, 40000, 1000];
-let goodSales = sales.filter((sales) => sales >= 17000);
+// let sales = [2100, 25000, 17000, 10000, 40000, 1000];
+let sales = [200, 500, 300, 800, 600];
+
+let goodSales = sales.filter((sales) => sales > 500);
 
 console.log(goodSales);
